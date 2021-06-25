fb9562e74847914ab4df710af4a1b5055c0f6082
Maria <imgvoid@gmail.com>
2021-06-25 22:28:27 +0300

    templates

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('products/<slug:category>/', views.CategoryListView.as_view(), name='category-list'),
     path('products/', views.ProductListView.as_view(), name='product-list'),
     path('product/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
