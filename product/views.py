34d03e1b10db8336f2615c3bace33b25f21363a8
Maria <imgvoid@gmail.com>
2021-06-25 21:15:24 +0300

    signup

--- product/views.py
+++ product/views.py
@@ -95,19 +95,4 @@ def signup(request):
             return redirect('product-list')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
-
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('product-list')
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {'form': form})
