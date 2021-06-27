48eb3986faf63518a1b4a918016551eb3a558e5e
Maria <imgvoid@gmail.com>
2021-06-27 23:10:04 +0300

    templates

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('cart_add/<str:slug>/', views.CartAdd.as_view(), name='cart-add'),
+    path('cart_add/<str:slug>/<int:quantity>/', views.CartAdd.as_view(), name='cart-add'),
+    path('cart_delete/<str:slug>/<int:quantity>', views.CartRemove.as_view(), name='cart-remove'),
     path('cart_get/', views.CartGet.as_view(), name='cart-get'),
+    path('cart/', views.CartView.as_view(), name='cart-view')
 ]
--- order/views.py
+++ order/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.list import ListView
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect
 from . import models
 from django.views import View
 from django.urls import reverse
 from product.models import Product
-from .models import ShopCart, OrderProduct
 
 
 # Create your views here.
@@ -15,12 +16,24 @@ class CartAdd(View):
     def get(self, request, *args, **kwargs):
         cart = request.session['cart']
         try:
-            cart[self.kwargs['slug']] += 1
+            cart[self.kwargs['slug']] += self.kwargs['quantity']
         except KeyError:
             cart[self.kwargs['slug']] = 1
         request.session.modified = True
 
-        return redirect(reverse('product-list'))
+        return HttpResponseRedirect(self.request.META.get('HTTP_REFERER'))
+
+
+class CartRemove(View):
+    def get(self, request, *args, **kwargs):
+        cart = request.session['cart']
+        if self.kwargs['quantity'] < cart[self.kwargs['slug']]:
+            cart[self.kwargs['slug']] -= self.kwargs['quantity']
+        else:
+            del cart[self.kwargs['slug']]
+        request.session.modified = True
+
+        return redirect(reverse('cart-view'))
 
 
 class CartGet(View):
@@ -33,3 +46,25 @@ class CartGet(View):
         cart_products = [item['product_slug'] for item in cart]
 
         return JsonResponse(cart)
+
+
+class CartView(ListView):
+    template_name = 'shopping_cart.html'
+
+    def get_queryset(self):
+        cart = self.request.session['cart']
+        products = Product.objects.filter(slug__in=cart.keys())
+        order_list = [{'product': product, 'quantity': cart[product.slug],
+                       'total_price': product.price * cart[product.slug]}
+                      for product in products]
+        self.kwargs['order_list'] = order_list
+        return order_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        price_list = [product['quantity'] * product['product'].price for product in self.kwargs['order_list']]
+        context['price_list'] = price_list
+        context['total_price'] = sum([item['total_price'] for item in self.kwargs['order_list']])
+        return context
+
+
